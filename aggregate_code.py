4a2cc1b2ccf7acbf931ccfe0c4163426158cc11f
EtiNL <etienne.lanzeray@gmail.com>
2024-06-27 11:29:12 +0200

    modified:   aggregate_code.py

--- aggregate_code.py
+++ aggregate_code.py
@@ -9,7 +9,7 @@ def aggregate_code_from_folder(folder_path):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             # Check if the file has a .rs or .py extension and is not in a 'target' folder
-            if (file.endswith('.rs') or file.endswith('.py')) and 'target' not in root.split(os.sep):
+            if (file.endswith('.rs') or file.endswith('.py') or file.endswith('.cu') or file.endswith('Dockerfile')) and 'target' not in root.split(os.sep):
                 file_path = os.path.join(root, file)
                 with open(file_path, 'r', encoding='utf-8') as f:
                     file_content = f.read()
